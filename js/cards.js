34e44e73f53eb13434b2fee151778e507a901c26
WolfY <103885166+xWolfYx@users.noreply.github.com>
2025-05-07 18:48:13 +0400

    feat: add initial js files

---
+++ js/cards.js
@@ -0,0 +1,57 @@
+// gifts
+
+function shuffleCards(array) {
+  for (let i = 0; i < array.length - 1; i += 1) {
+    const j = Math.floor(Math.random() * array.length);
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+const categoryImg = {
+  "For Work": "../images/balls/gift-for-work.png",
+  "For Health": "../images/balls/gift-for-health.png",
+  "For Harmony": "../images/balls/gift-for-harmony.png",
+};
+
+const giftContainer = document.getElementById("cards");
+
+fetch("../js/gifts.json")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Failed to fetch: " + response.statusTest);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    shuffleCards(data);
+    data.forEach((item) => {
+      const card = document.createElement("div");
+      card.className = "card";
+
+      const cardImage = document.createElement("div");
+      cardImage.className = "card-image";
+
+      const img = document.createElement("img");
+      img.src = categoryImg[item.category];
+      img.alt = item.category;
+      cardImage.appendChild(img);
+
+      card.appendChild(cardImage);
+
+      const cardText = document.createElement("div");
+      cardText.className = "card-text";
+
+      const header4 = document.createElement("h4");
+      header4.className = `header-4 ${item.category.toLowerCase().replace(" ", "-")}`;
+      header4.textContent = item.category;
+      cardText.appendChild(header4);
+
+      const header3 = document.createElement("h3");
+      header3.className = "header-3";
+      header3.textContent = item.name;
+      cardText.appendChild(header3);
+      card.appendChild(cardText);
+
+      giftContainer.appendChild(card);
+    });
+  });
---
+++ js/index.js
@@ -0,0 +1,42 @@
+// burger menu activation script
+const burgerMenu = document.getElementById("burger-menu-icon");
+const burgerFirstLine = burgerMenu.firstElementChild;
+const burgerSecondLine = burgerMenu.lastElementChild;
+let active = false;
+function toggleBurgerMenu() {
+  const burgerMenu = document.getElementById("burger-menu");
+  if (!active) {
+    burgerFirstLine.classList.add("close1");
+    burgerSecondLine.classList.add("close2");
+    burgerMenu.style.width = "100%";
+    document.body.style.overflow = "hidden";
+    active = true;
+  } else {
+    burgerFirstLine.classList.remove("close1");
+    burgerSecondLine.classList.remove("close2");
+    document.body.style.overflow = "visible";
+    burgerMenu.style.width = "0";
+    active = false;
+  }
+}
+
+// the button to scroll up on tablets and mobile phones
+window.onscroll = function () {
+  showScrollButton();
+};
+const topScrollButton = document.getElementById("top-btn");
+function showScrollButton() {
+  if (document.documentElement.scrollTop > 300) {
+    topScrollButton.style.visibility = "visible";
+    topScrollButton.style.opacity = "1";
+  } else {
+    topScrollButton.style.visibility = "hidden";
+    topScrollButton.style.opacity = "0";
+  }
+}
+topScrollButton.onclick = function () {
+  scrollUp();
+};
+function scrollUp() {
+  document.documentElement.scrollTop = 0;
+}
